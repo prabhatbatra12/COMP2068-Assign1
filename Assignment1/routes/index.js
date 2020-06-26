342c1fbacb1520eb247c625ba91851efd3c0d6c8
prabhatbatra12 <45927420+prabhatbatra12@users.noreply.github.com>
2020-06-25 23:07:15 -0400

    Add project files.

---
+++ Assignment1/routes/index.js
@@ -0,0 +1,19 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+router.get('/', function (req, res) {
+    res.render('Home', { title: 'Prabhat Home' });
+});
+router.get('/about_me', function (req, res) {
+    res.render('about_me', { title: 'About Me' });
+});
+router.get('/projects', function (req, res) {
+    res.render('projects', { title: 'Comp2068 - Projects' });
+});
+router.get('/services', function (req, res) {
+    res.render('services', { title: 'Services' });
+});
+router.get('/contact_me', function (req, res) {
+    res.render('contact_me', { title: 'Contact info' });
+});
+module.exports = router;
